aa783d3e562661190effa54e66d57d7004ccdd37
Marvin Arnold <marvinmarnold@gmail.com>
2016-10-03 22:50:58 -0500

    touch spacing

--- common/lbs-lookups.js
+++ common/lbs-lookups.js
@@ -1,4 +1,4 @@
 import LbsLookupsSchema from './lbs-lookups-schema.js';
 
-export const LbsLookups= new Mongo.Collection("LbsLookups");
+export const LbsLookups = new Mongo.Collection("LbsLookups");
 LbsLookups.attachSchema(LbsLookupsSchema);
